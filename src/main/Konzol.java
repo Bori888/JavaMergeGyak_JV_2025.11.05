e003b0faf52cb4ae973d264b937ab286406b882c
IlyésBorbála(SZF_202 <IlysBorbla(SZF_202@Tanulo-227-16>
2025-11-05 14:10:33 +0100

    kiiras

--- src/main/Konzol.java
+++ src/main/Konzol.java
@@ -4,9 +4,9 @@ package main;
 
 public class Konzol {
 
-   
+ 
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Kiirás");
     }
     
 }
